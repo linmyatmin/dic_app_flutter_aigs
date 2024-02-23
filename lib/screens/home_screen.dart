ce820d41b8edacbba86812a980826b34c4221103
linmyatmin <lin@aigsthailand.com>
2024-02-23 09:18:20 +0700

    UPDATE APP TITLE

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
         title: const Text(
-          "sdfsdfsdf",
+          "GEMPEDIA",
           style: TextStyle(color: Colors.white),
         ),
         actions: <Widget>[
